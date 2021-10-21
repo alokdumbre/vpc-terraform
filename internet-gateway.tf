771feff4af3e355e616e8d9c07ac91c02d3c7f62
alokdumbre <56631296+alokdumbre@users.noreply.github.com>
2021-10-21 06:25:51 +0000

    Delete internet-gateway.tf

--- internet-gateway.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_internet_gateway" "main" {
-  # The VPC ID to create in.
-  vpc_id = aws_vpc.main.id
-  # A map of tags to assign to the resource.
-  tags = {
-    Name = "sbp-IGW"
-  }
-}
